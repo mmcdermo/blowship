c58ccd435a7c774a0586c378c702931bee8cee25
Morgan McDermott <morgan@morganmcdermott.com>
2014-02-21 22:27:09 -0800

    Running, not working

--- pirgroup.py
+++ pirgroup.py
@@ -1,6 +1,9 @@
 import ntheory
 import primes
-import pohlig_hellman
+import ph
+import factoring
+import crt
+from math import log, ceil, floor
 #Gi is the set of cyclic groups whose order is a number in [2^k, 2^(k+1)] and is divisible by pi_i
 
 def SampleG(i):
@@ -25,31 +28,33 @@ def genPrimes(n):
 def genPis(l, P):
     S = []
     for i in range(0, len(P)):
-        c_i = (l / log(P[i], 2)).ceil()
-        pi[i] = P[i] ** c_i
+        c_i = ceil(l / log(P[i], 2))
+        S.append(P[i] ** c_i)
     return S
 
 def securityConstants():
     #Params we need: kprime, f1, f2
-    #kprime > b * log(n)
+    # kprime > b * log(n)
+    # k = Omega(log^(3-o(1)) n) to prevent number field sieve
     b = 2 # security! TODO
     def f1(x, y): return x * y #security! TODO
     def f2(x, y): return x * y #security! TODO
-    return (b * log(n, 2), f1, f2)
+    return (b * ceil(log(n, 2)), f1, f2)
 
-def setup(n):
+def setup(n, l):
     (kprime, f1, f2) = securityConstants()
     k = f2(kprime, log(n, 2)) #Security param
-    l = (f1(k, log(n, 2))).floor() #Security integer
+    #l = floor(f1(k, log(n, 2))) #Security integer
+
     # given default sec constants, l is (6 log(n))
-    t = (n / l).ceil()  #Size of S and P
-    P = genPrimes(t)    #Set of small primes
+    t = ceil(n / l)  #Size of S and P
+    P = genPrimes(int(t))    #Set of small primes
     S = genPis(l, P)    #Set of prime powers, all > 2^l
     return {'k':k, 'l':l, 't':t, 'P':P, 'S':S}
 
 # Generate a modulus m that phi-hides pi_i [pi_i divides phi(m)]
 #   (this is how we define the cyclic group G)
-def generateM(pi_i):
+def generateMSecure(pi_i):
     # Choose random "semi-safe" prime Q_0 = 2(q_0)(pi_i) + 1
     #   and random "semi-safe" prime Q_1 = 2d(q_1) + 1
     #   then m = (Q_0)(Q_1)
@@ -62,7 +67,7 @@ def generateM(pi_i):
     d = primes.primes[150]
     Q0 = 4
     i = 100
-    while not primes.is_prime(Q0):
+    while not primes.is_prime(int(Q0)):
         q_0 = primes.primes[i]
         i = i + 1
         Q0 = 2 * (q_0) * (pi_i) + 1
@@ -75,21 +80,76 @@ def generateM(pi_i):
     m = Q0 * Q1
     return m
 
-def generateQuery(i, f1, f2, S, D, ki1):
-    m = generateM(S[i])
-    g = 1 #random element of G s.t. GCD( |G:<g>|, product of all p_j's ) = 1
-    q = m / S[i] # q = |G| / pi_i
-    h = g ** q   # Will use this to process the response
-    return ( (G,g), h, q )
+#Unlike the previous method, this one produces an integer value
+#  that is divisible by pi_i
+def generateM(pi_0, pi_i):
+    #q should be sampled from
+    # [2^k', 2^k' + 1] and be a factor/divisor? of pi_0 * pi_1
+    f = factoring.factor( pi_0 * pi_i )
+    q = f[0]
 
-def generateResponse(m, B, S):
-    e = chinese_remainder_theory(zip(B, S))
-    g_e = pow(g, e, m) #g_e = g^e mod m
+    #d should be sampled from
+    # [2^k / (q * pi_i), 2^(k+1) / (q * pi_i) ]
+    # where all prime divisors of d are less than q
+    d = primes.primes[150]
+    
+    #Finally, we should return |G| = m = pi_i * q * d
+    m = pi_i * q * d
+
+    return int(m)
+
+def generateQuery(i, S):
+    g = primes.primes[40] #random element of G s.t. GCD( |G:<g>|, product of all p_j's ) = 1
+    m = generateM(S[0], S[i])
+    q = int(m / S[i])  # q = |G| / pi_i [[ Should be an integer ]]
+    h = pow(g, q, m)   # Will use this to process the response
+    return {'g': g, 'h': h, 'q': q, 'm': m}
+
+def generateResponse(m, g, B, S):
+    (e, _) = crt.ChineseRemainder(zip(B, S))
+    g_e = pow(g, int(e), m) #g_e = g^e mod m
     return g_e
 
-def processResponse(g_e, q, h):
+def processResponse(g_e, q, h, m):
     h_e = pow(g_e, q, m)
-    C_i = phlog(h, h_e, m)
-      #Above: C_i is the discrete logarithm log_h(h_e) within the subgroup
-      # H \subset G of order pi_i = p_i^(c_i) 
+    C_i = ph.PohligHellmanModP(h, h_e, m)
+    #Above: C_i is the discrete logarithm log_h(h_e) within the subgroup
+    # H \subset G of order pi_i = p_i^(c_i) 
     return C_i
+
+#Establish test block values
+C = [100, 101, 102, 103, 104, 105]
+
+#Get an upper bound on block size
+max = 0
+for i in range(0, len(C)):
+    l = ceil(log(C[i], 2))
+    if l > max: max = l
+l = 2 * max
+
+#Get database size
+n = len(C) * l
+
+print "Database: " + str(C)
+print "l (chunk length): " + str(l)
+print "n (database size): " + str(n)
+
+params = setup(n, l)
+print "Parameters: " + str(params)
+
+#Prime power size security test
+pow2l = str(2 ** l)
+for i in range(0, len(params['P'])): 
+    if(params['P'][i] >= pow2l): print "Prime Power too small"
+
+#Generate a query
+i = 2
+query = generateQuery(i, params['S'])
+print "Query: " + str(query)
+
+#Generate a response
+response = generateResponse(query['m'], query['g'], C, params['S'])
+print "Response: " + str(response)
+
+#Process Response
+proc_response = processResponse(response, query['q'], query['h'], query['m'])
