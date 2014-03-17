d9cdd7052472793f1b71e71bf17c14a481b58698
stealthycoin <john.w.carlyle@gmail.com>
2014-03-17 14:23:30 -0700

    Comments

--- server/Client.py
+++ server/Client.py
@@ -11,6 +11,10 @@ lock = threading.RLock()
 host = socket.gethostname()
 
 def readFromSocketUntil(s, signal="END"):
+    """
+    Takes a socket s and a terminating signal to listen for
+    and reads all data up until the signal
+    """
     data_from_server = ""
     block = ""
     while block.find(signal) == -1:
@@ -43,6 +47,9 @@ def encrypt(message, n, e, userId, svrkey):
     return msg
     
 def sendData(data):
+    """
+    Send data to the server without using the same socket as the user input
+    """
     tsock = socket.socket()
     port = 8000
     tsock.connect((host,port))
@@ -52,12 +59,17 @@ def sendData(data):
     tsock.close()
 
 def generateCircuit(n, enduser, message, keys, online, svrkeys):
+    """
+    Generates a circuit to send a message through given an end recepient
+    """
+
     if enduser not in keys.keys():
         return None
 
     #first encrypt for the intended recipient
     ct = encrypt(message, keys[enduser][1], keys[enduser][0], enduser, svrkeys)
 
+    #layer on more encryptions to more users based on the desired circuit length
     length = 1
     while length < n:
         user = random.choice(online)
@@ -113,10 +125,12 @@ def user(name, p, msgs, ned):
 
             print "Downloaded " + str(len(keys.keys()))  + " keys"
 
+        #request a list of users from the server
         if words == ":ls":
             s.send(words)
             online = ast.literal_eval(readFromSocketUntil(s))
             print "Online users: %s" % online
+        #send a message to the server
         if words.startswith(":send"):
             words = words[6:]
             index = words.find(" ")
@@ -127,17 +141,18 @@ def user(name, p, msgs, ned):
                 print "Do not have that user's key, try :ks"
             else:
                 sendData(circuit)
-
+        #send the server a key
         if words.startswith(":setkey"):
             with lock:
                 s.send(":setkey " + str(ned[1])+"n"+str(ned[0]))
             print "Keys: %s" % s.recv(1024)
+        #generate a new keypair
         if words.startswith(":genkey"):
             with lock:
                 ned = rsa.newKey(10**100,10**101,50)
             print "n = %s\ne = %s\nd = %s\n" % (ned[0],ned[1],ned[2])
             with open('%s.keys' % name, 'w') as f:
-                f.write("%d\n%s\n%s\n" %(ned[0],ned[1],ned[2]))
+                f.write("%d\n%s\n%s\n" % (ned[0],ned[1],ned[2]))
 
     s.close()
     p.terminate() #kill the worker thread
@@ -187,6 +202,7 @@ if __name__ == "__main__":
     except:
         pass
     
+    #thread safe list for communicating between threads
     manager = Manager()
     msgs = manager.list()
     
@@ -198,7 +214,10 @@ if __name__ == "__main__":
         ned = (1,1,1)
         print "No keys found, consider making some with :genkey"
 
+    #create and start the cuircuit thread in the background
+    #for processing messages from the server
     p = Process(target=circuit,args=(name,msgs,ned))
     p.start()
+    #start user thread that listens for user input
     user(name, p, msgs, ned)
     
--- server/Server.py
+++ server/Server.py
@@ -1,3 +1,8 @@
+##################
+# Blow ship server
+#################
+#Proof of concept, not feature complete.
+
 import ast
 import signal
 import cPickle as pickle
@@ -11,6 +16,9 @@ import sys
 lock = threading.RLock()
 
 def process_client(c, ID_KEY, ID_STATUS, ID_SOCK, ned):
+    """
+    This function servs as a thread that listens for user input
+    """
     c_id = c.recv(1024)
     print c_id + " logged in"
     with lock:
@@ -19,22 +27,27 @@ def process_client(c, ID_KEY, ID_STATUS, ID_SOCK, ned):
 
     c.send(str(ned[1])+"n"+str(ned[0]))
 
+    #listen for user input and respond if need be
     running = True
     while running:
         v = c.recv(1024)
+        #end user wishes to log out
         if v == ":bye":
             print c_id + " logged out"
             running = False
             c.send("bye")
+        #user is sending their public key and n value
         elif v.startswith(":setkey"):
             v = v[7:]
             with lock:
                 ID_KEY[c_id] = v
             c.send("reset key to: " + v)
             print c_id + " sent a new public key " + v
+        #user wants to know who is online
         elif v.startswith(":ls"):
             print "list requested, sending: %s" % ID_STATUS
             c.send("%sEND" % filter(lambda x: ID_STATUS.get(x, False)  == True,ID_STATUS.keys()))
+        #user wants to know all other users keys (including their own)
         elif v.startswith(":ks"):
             print "keys requested, sending: %s" % ID_KEY
             c.send("%sEND" % ID_KEY)
@@ -47,8 +60,16 @@ def process_client(c, ID_KEY, ID_STATUS, ID_SOCK, ned):
 
 
 def process_circuit(c, ID_KEY, ID_STATUS, ID_SOCK, ned):
+    """
+    This thread moniters
+    """
     c_id = c.recv(1024)
 
+    #every second check if there is a message for user c_id
+    #if so send it to them through their circuit socket
+    #they will recieve decrypt the message and either
+    #1) Keep it if it belongs to them
+    #2) Send it back to the server through a new data connection if they cannot read it
     running = True
     while running:
         time.sleep(1)
@@ -79,8 +100,6 @@ def process_data(c, ID_SOCK, ned):
     d += block
     d = d[:d.find("END")]
 
-    print "Process '%s'" % d
-    
     #reformat to integer list
     d = pickle.loads(str(d))
     d = rsa.decrypt(d, ned[0], ned[2], 15)
@@ -95,27 +114,32 @@ def main(data):
     """
     Launches the server with given set of keys (data)
     """
+    #this library needs to be replaced
     ned = rsa.newKey(10**100,10**101,50)
     print "Keys\nn = %s\ne = %s\nd = %s\n" % (ned[0],ned[1],ned[2])
 
+    #create thread safe dictionaries
     manager = Manager()
     ID_KEY = manager.dict()
     ID_KEY.update(data) #get saved data
     ID_STATUS = manager.dict()
     ID_SOCK = manager.dict()
 
-    #define an interupt catcher to save data (doesnt work)
+    #define an interupt catcher to save data
     def signal_handler(signal, frame):
         print('Saving data')
+        #this is ugly but the only reasonable way I could find
+        #to unroll a managed threadsafe dictionary to a normal one
+        #that can interact with pickle correctly
         with lock:
             with open('serverdata.pkl', 'wb') as f:
                 temp = ast.literal_eval(str(ID_KEY))
-                pickle.dump( temp, f ) #save keys on edit
+                pickle.dump( temp, f ) #save the keys
         sys.exit(0)
     signal.signal(signal.SIGINT, signal_handler)
 
+    #listen on port 8000 (probably a bad choice, usually used for testing web servers) for incoming connections
     s = socket.socket()
-
     hostname = socket.gethostname()
     port = 8000
     s.bind(('',port))
@@ -127,18 +151,24 @@ def main(data):
         c.send("init")
         v = c.recv(4)
         if "user" == v:
+            #incomming connection is a user which will send queries
             Process(target=process_client, args=(c,ID_KEY,ID_STATUS,ID_SOCK, ned)).start()
         elif "circ" == v:
+            #incoming connection is to be used as part of a circuit
             Process(target=process_circuit, args=(c,ID_KEY,ID_STATUS,ID_SOCK,ned)).start()
         elif "data" == v:
+            #incoming connection has some data for the server to process
             Process(target=process_data, args=(c,ID_SOCK,ned)).start()
 
 
 if __name__ == "__main__":
-    try:
+    """
+    Main function reads previous keys if they exist before launcing the main listening thread
+    """
+    try: #try to read key data
         with open('serverdata.pkl', 'rb') as f:
             data = pickle.load(f)
-    except:
+    except: #data failed to read
         print "Data not found"
         data = {}
 
